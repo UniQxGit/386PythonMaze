a563c7ed1592c177c526277aefcb02dd2c291e4a
UniQx <31258420+UniQxGit@users.noreply.github.com>
2018-02-06 19:59:52 -0800

    Open portal on random line working. Note: There's Recursive call syntax error i've commented out.

--- helper.py
+++ helper.py
@@ -13,7 +13,36 @@ def build_maze(arr = [], cornerCellX = 0, cornerCellY = 0, width = 1, height = 1
 		return arr
 	vertical = width > height
 	splitLine = cornerCellX+random.randint(1,width-1)*2 if vertical else cornerCellY+random.randint(1,height-1)*2
+	
 	arr[splitLine][cornerCellY] = "o"
 	print ("From builder:\n","Vertical:",vertical," splitLine:",splitLine)
+	
+	portal = random.randint(1,height)*2-1 if vertical else random.randint(1,width)*2-1;
+
+	if(vertical):
+		arr[portal][splitLine] = " " #Replace with space
+
+		widthOfFirstHalf = splitLine/2
+		widthOfLasthalf = width - widthOfFirstHalf
+		heightOfFirstHalf = height
+		heightOfLasthalf = height
+
+		newCornerCellX = splitLine
+		newCornerCellY = cornerCellY
+	else:
+		arr[splitLine][portal] = " " #Replace with space
+
+		#Set new width/heights
+		widthOfFirstHalf = width
+		widthOfLasthalf = width
+		heightOfFirstHalf = splitLine/2
+		heightOfLasthalf = height - heightOfFirstHalf
+
+		newCornerCellX = cornerCellX
+		newCornerCellY = splitLine
+
+	#arr = build_maze(arr,cornerCellX=cornerCellX,cornerCellY=cornerCellY,width=widthOfFirstHalf,height=heightOfFirstHalf) #first half
+	#arr = build_maze(arr,cornerCellX=newCornerCellX,cornerCellY=newCornerCellY,width=widthOfLastHalf,height=heightOfLastHalf) #last half
+
 	return arr
 
